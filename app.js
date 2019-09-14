63ad46a9cb81c72673d2a474d4c50aebab8d49c6
Aaron Meder <public.aaron@gmail.com>
2019-09-14 10:32:12 +0200

    Simple speedtest using speedtest-net

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const cron = require('node-cron');
 const express = require('express');
+const speedTest = require('speedtest-net');
+
+const test = speedTest({
+  maxTime: 20000,
+});
 
 const app = express();
 
 // schedule tasks to be run on the server
-cron.schedule('* * * * * *', function() {
-  console.log('running a task every second');
+cron.schedule('* * * * *', function() {
+  console.log('-- task started ----');
+
+  // check speed
+  // -------------
+
+  // speedtest successful
+  test.on('data', data => {
+    console.dir(data);
+  });
+
+  // speedtest failed
+  test.on('error', err => {
+    console.error(err);
+  });
 });
 
 app.listen(3128);
